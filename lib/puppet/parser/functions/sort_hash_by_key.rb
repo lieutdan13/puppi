2c8b28a1553389b6535427af6f5013553be16921
Dan Schaefer <dan@schaeferzone.net>
2013-11-19 12:47:43 -0500

    Adding a function to sort a hash by keys, with an option to sort recursively

---
+++ lib/puppet/parser/functions/sort_hash_by_key.rb
@@ -0,0 +1,14 @@
+module Puppet::Parser::Functions
+  newfunction(:sort_hash_by_key) do |args|
+    my_hash = args[0]
+    recursive = args[1]
+
+    my_hash.keys.sort().reduce({}) do |seed, key|
+      seed[key] = my_hash[key]
+      if recursive && seed[key].is_a?(Hash)
+        seed[key] = sort_hash_by_key(seed[key], true)
+      end
+      seed
+    end
+  end
+end
